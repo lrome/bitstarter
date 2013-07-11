be666f6db18cb640d2a4ac99ef1d6f9c5152fef2
Luis Romero <luigi.romer@gmail.com>
2013-07-11 06:00:38 -0700

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buf; 
+
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  buf = data;
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
