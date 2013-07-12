a3d9a0cbecaece5ffe8a229afe7628b51241166e
Luis Romero <luigi.romer@gmail.com>
2013-07-11 20:26:25 -0700

    Fix problems.

--- web.js
+++ web.js
@@ -1,16 +1,13 @@
+#!/usr/bin/env node
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf; 
-
-fs.readFileSync('./index.html', function (err, data) {
-  if (err) throw err;
-  buf = data;
-});
+var data = fs.readFileSync('index.html');
+var buf = new Buffer(data);
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8"));
+    response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
